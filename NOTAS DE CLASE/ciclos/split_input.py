7ccc83d93126a265158c5d3c326d323dfb4eee95
gitsselcc <yeimicc@lcg.unam.mx>
2025-03-21 13:09:02 -0600

    cambios guardados

---
+++ NOTAS DE CLASE/ciclos/split_input.py
@@ -0,0 +1,8 @@
+numeros_str = input("Dame tres numeros separados por espacio:").split()
+print(numeros_str)
+suma= sum(numeros_str)
+print(suma)
+
+num= [1,2,3]
+suma_numeros= sum(num)
+print(suma_numeros)
