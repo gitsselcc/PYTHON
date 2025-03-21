45c84b59985234e3d0fecb8aa5eb052cac3ce1ca
gitsselcc <yeimicc@lcg.unam.mx>
2025-03-21 13:33:52 -0600

    Add "tamano"

--- NOTAS DE CLASE/ciclos/split_input.py
+++ NOTAS DE CLASE/ciclos/split_input.py
@@ -4,6 +4,7 @@
 #print(numeros_str)
 #suma= sum(numeros_str)
 #print(suma)
+#CODIGO FEO
 
 
 numeros_str = input("Dame tres números separados por espacio: ").split()
@@ -11,3 +12,4 @@ numeros = list(map(int, numeros_str))  # Convertimos las cadenas a enteros
 print(numeros)  # Imprime la lista de números como enteros
 suma = sum(numeros)  # Calcula la suma de los números
 print(f"La suma es: {suma}")
+#CODIGO PRO
