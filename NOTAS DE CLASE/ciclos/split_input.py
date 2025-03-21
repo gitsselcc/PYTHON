85fc6ad91aa79cc7660f8ff04e3f6ecf01f2b5ec
gitsselcc <yeimicc@lcg.unam.mx>
2025-03-21 13:10:39 -0600

    Cambios STR

--- NOTAS DE CLASE/ciclos/split_input.py
+++ NOTAS DE CLASE/ciclos/split_input.py
@@ -1,8 +1,14 @@
-numeros_str = input("Dame tres numeros separados por espacio:").split()
-print(numeros_str)
-suma= sum(numeros_str)
-print(suma)
+#numeros_str = input("Dame tres numeros separados por espacio: ").split()
+#print(map(int,numeros_str))
 
-num= [1,2,3]
-suma_numeros= sum(num)
-print(suma_numeros)
+#print(numeros_str)
+#suma= sum(numeros_str)
+#print(suma)
+
+
+numeros_str = input("Dame tres números separados por espacio: ").split()
+numeros = list(map(int, numeros_str))  # Convertimos las cadenas a enteros
+
+print(numeros)  # Imprime la lista de números como enteros
+suma = sum(numeros)  # Calcula la suma de los números
+print(f"La suma es: {suma}")
