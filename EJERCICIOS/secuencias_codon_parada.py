0cbac77d716ff94396547b4b6c652cf8c800f1b8
gitsselcc <yeimicc@lcg.unam.mx>
2025-03-28 14:05:46 -0600

    EJERCICIOS/codon_de_parada

---
+++ EJERCICIOS/secuencias_codon_parada.py
@@ -0,0 +1,14 @@
+# Pedir al usuario que ingrese las secuencias de ADN separadas por comas
+dna_input = input("Introduce las secuencias de ADN separadas por comas: ").split(',')
+
+# Filtrar las secuencias que contienen un codón de parada
+secuencias_codon_parada = [sequence for sequence in dna_input if 'TAA' in dna_input or 'TAG' in dna_input or 'TGA' in dna_input] 
+
+# Imprimir las secuencias que contienen codones de parada
+print("Las secuencias que contienen un codón de parada son:")
+print(secuencias_codon_parada)
+
+
+#Primero colocar un imput, asignandole a este un =a variable para manejarlo mas adelante.
+#El input debe pedir las secuencias de DNA separadas por comas, antes de procesar esto, eliminamos las comas con split.
+#Filtramos las secuencias que tienen codon de parada, primero le asiganmos una variable a nuestra lista, dentro hacemos el ciclo for, y ponemos la condicional
