b9f17786394524568f60626c746e319866216803
gitsselcc <yeimicc@lcg.unam.mx>
2025-03-28 14:06:32 -0600

    CORRECCION: codones_inicio

--- EJERCICIOS/codones_inicio.py
+++ EJERCICIOS/codones_inicio.py
@@ -1,5 +1,5 @@
 secuencias = input("Dame secuencias separadas por comas: ").split(',')
 
-codones_inicio = (secuencia [:3] for secuencia in secuencias)
+codones_inicio = [secuencia [:3] for secuencia in secuencias]
 
 print (codones_inicio)
