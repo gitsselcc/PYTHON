5578d1cfa976f613f9743137c28a259a02eb5a05
gitsselcc <yeimicc@lcg.unam.mx>
2025-04-04 13:59:09 -0600

    TUPLAS/extraer fragmentos

---
+++ bin/TUPLAS_DICCIONARIOS/extraer_fragmentos.py
@@ -0,0 +1,8 @@
+secuencia = "ATGCGTAGC"
+tupla = tuple(secuencia)
+primeros_cinco = tupla[0:5]
+print (primeros_cinco)
+ultimos_tres = tupla [-3:]
+print (ultimos_tres)
+secuencia_invertida = tupla [::-1]
+print (secuencia_invertida)
