09cd348d366274501282b116a00614d3dea01a94
gitsselcc <yeimicc@lcg.unam.mx>
2025-04-04 13:57:17 -0600

    Adicion de Archivos a PRACTICAS

---
+++ bin/PRACTICAS/expresion = 5.py
@@ -0,0 +1,8 @@
+expresion = 5.2  # Cambia este valor
+
+if expresion > 10:
+    print("Gen sobreexpresado")
+elif 2 <= expresion <= 10:
+    print("Expresión normal")
+else:
+    print("Baja expresión")
