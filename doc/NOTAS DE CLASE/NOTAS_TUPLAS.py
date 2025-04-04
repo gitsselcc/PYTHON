a310976476101bab9991a554599adc3e747ce68c
gitsselcc <yeimicc@lcg.unam.mx>
2025-04-04 13:59:48 -0600

    Notas de TUPLAS/data/Notas de Clase

---
+++ doc/NOTAS DE CLASE/NOTAS_TUPLAS.py
@@ -0,0 +1,11 @@
+# Lo que indifica que es un objeto es la coma, de esta manera separaramos nuestros elementos
+
+tupla_vacia = ()
+tupla_numeros = (1,2,2,4,5)
+mi_tupla = 1,2,3
+una_tupla = (5,)
+no_es_tupla = (5)
+tupla_mixta = (1, "hola", 3.14, True)
+tupla_anidada = ((1,2,3),(4,5,6))
+tupla_compleja = (1, "Python", 3.14, [1,2,3])
+
